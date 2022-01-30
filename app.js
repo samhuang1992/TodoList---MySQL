681964300517ede0df01c328e13bb7a74956dff9
samhuang1992 <bob60117@gmail.com>
2022-01-30 16:24:23 +0800

    feat: add migration

--- app.js
+++ app.js
@@ -1,19 +1,92 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
 const {engine} = require('express-handlebars')
-const bodyParser = require('body-parser') //不用額外載入了
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+// passport設定檔，要寫在 express-session後
+const usePassport = require('./config/passport')
+const db = require('./models')
+const User = db.User
+const Todo = db.Todo
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'MySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
