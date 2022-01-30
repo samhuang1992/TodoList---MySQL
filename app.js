c7c00b09f90eaa4b0a876527cf17937c4ca85ad4
samhuang1992 <bob60117@gmail.com>
2022-01-30 12:59:47 +0800

    project init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const {engine} = require('express-handlebars')
+const bodyParser = require('body-parser') //不用額外載入了
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
