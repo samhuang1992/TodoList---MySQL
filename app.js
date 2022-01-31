6887f60b69a712a109a388f273c46e846233d161
samhuang1992 <bob60117@gmail.com>
2022-01-31 23:54:13 +0800

    feat: message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passport = require('passport')
 const {engine} = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
 // passport設定檔，要寫在 express-session後
 const usePassport = require('./config/passport')
 const db = require('./models')
@@ -27,66 +28,14 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
-
-
-
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
+app.use(flash())
+// 設定變數res.locals 
+app.use((req, res, next)=> {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') //設定成功訊息
+  res.locals.warning_msg = req.flash('warning_msg') //設定錯誤訊息
+  next()
 })
 
 app.listen(PORT, () => {
